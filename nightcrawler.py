9ee306655a86a0373f1b8fa06cab9a9e32047a04
asmahler <asmahler@gmail.com>
2017-12-14 17:55:00 -0700

    updated and now running fast!

--- nightcrawler.py
+++ nightcrawler.py
@@ -1,11 +1,11 @@
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
 from multiprocessing.dummy import Pool as ThreadPool
+from concurrent.futures import ThreadPoolExecutor, wait, as_completed
 from pyfiglet import figlet_format
 import requests
 import datetime
 import threading
-import imap
-import grequests
 
 print(figlet_format("NightCrawler"))
 
@@ -13,25 +13,26 @@ sitemap = []
 good_urls = []
 bad_urls = []
 none_urls = []
-base_url = "https://testazure.drivetime.com"
+base_url = "https://www.weebly.com"
 
 start_time = datetime.datetime.now()
-main_sitemap = requests.get(base_url + "/sitemap-main.xml")
-dsr_sitemap = requests.get(base_url + "/sitemap-dl.xml")
-ddp_sitemap = requests.get(base_url + "/sitemap-dd.xml")
-some_vehicle_sitemap = requests.get(base_url + "/sitemap-vsr-3109509.xml")
+
+main_sitemap = requests.get(base_url + "/sitemap.xml")
+# dsr_sitemap = requests.get(base_url + "/sitemap/city-state-make-vehicles.xml")
+# ddp_sitemap = requests.get(base_url + "/sitemap-dd.xml")
+# some_vehicle_sitemap = requests.get(base_url + "/sitemap-vsr-3109509.xml")
 
 
 main_data = main_sitemap.text
-dsr_data = dsr_sitemap.text
-ddp_data = ddp_sitemap.text
-some_vehicles_data = some_vehicle_sitemap.text
+# dsr_data = dsr_sitemap.text
+# ddp_data = ddp_sitemap.text
+# some_vehicles_data = some_vehicle_sitemap.text
 
 
 main_soup = BeautifulSoup(main_data, 'html.parser')
-dealership_soup = BeautifulSoup(dsr_data, 'html.parser')
-ddp_soup = BeautifulSoup(ddp_data, 'html.parser')
-some_vehicle_soup = BeautifulSoup(some_vehicles_data, 'html.parser')
+# dealership_soup = BeautifulSoup(dsr_data, 'html.parser')
+# ddp_soup = BeautifulSoup(ddp_data, 'html.parser')
+# some_vehicle_soup = BeautifulSoup(some_vehicles_data, 'html.parser')
 
 def sitemap_builder(xml_sitemap):
 	sitemap_links = xml_sitemap.find_all("loc")
@@ -39,36 +40,32 @@ def sitemap_builder(xml_sitemap):
 		sitemap.append(loc.getText())
 	return sitemap
 
-def url_checker(url_status):
-	try: 
-		if url_status.status_code == 200:
-			good_urls.append(url_status)
-		else:
-			bad_urls.append(url_status.url)
-		return good_urls, bad_urls
-	except AttributeError: 
-		print("whoops this was an error: {0}".format())
+def url_checker(url_status): 
+	if url_status.status_code == 200: 
+		good_urls.append(url)
+	elif url_status.status_code == 404: 
+		bad_urls.append(url_status.url)
+	else: 
+		none_urls.append(url_status.url)
+	return good_urls, bad_urls, none_urls
 	
-			
 
 main_links = sitemap_builder(main_soup)
-dealership_links = sitemap_builder(dealership_soup)
-ddp_links = sitemap_builder(ddp_soup)
-some_vehicle_links = sitemap_builder(some_vehicle_soup)
+# dealership_links = sitemap_builder(dealership_soup)
+# ddp_links = sitemap_builder(ddp_soup)
+# some_vehicle_links = sitemap_builder(some_vehicle_soup)
 
 main_links.append(base_url + '/alexiscool')
 
-pool = ThreadPool(5)
+pool = ThreadPoolExecutor(50)
+futures = [pool.submit(requests.get,url) for url in main_links]
+results = [r.result() for r in as_completed(futures)] 
 
 
-rs = (grequests.get(link, timeout=2) for link in main_links)
-
-all_statuses = grequests.map(rs, stream=False, size=100, exception_handler=None, gtimeout=None)
-
-pool.map(url_checker, all_statuses)
-
-pool.close()
-pool.join()
+poolForLoop = ThreadPool(20)
+poolForLoop.map(url_checker, results) 
+poolForLoop.close()
+poolForLoop.join()
 
 
 end_time = datetime.datetime.now()
@@ -76,13 +73,11 @@ total_time = end_time - start_time
 
 print "You have checked: {0} Links!".format(len(main_links))
 print("WooHoo!!!: {0} Good URL's Checked".format(len(good_urls)))
-
-if none_urls: 
-	for url in none_urls: 
-		print("Ummm, this came back wrong, you might want to double check: {0}".format(url))
-
 if bad_urls:
 	for url in bad_urls: 
-		print('Oh No!! 404: {0}'.format(url)) 
+		print('Oh No!! 404: {0}'.format(url))
+if none_urls:
+	for url in bad_urls: 
+		print('Whoops, nothing found! Check this: {0}'.format(url)) 
 
 print "Total Time: {0}".format(total_time)
